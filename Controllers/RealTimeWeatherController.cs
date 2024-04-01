25ca06d2204c8bf19eeaeca6cf8ff4a70f2db735
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-04-01 14:01:32 +0200

    Add project files.

---
+++ Controllers/RealTimeWeatherController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherSite.Data;
+using WeatherSite.Models;
+
+namespace WeatherSite.Controllers
+{
+    public class RealTimeWeatherController : Controller
+    {
+        City city1 = new City();
+        private string CityName {  get; set; }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string cityName)
+        {
+            WeatherSite.Data.API api = new API();
+            city1.CityName = cityName;
+            string url = $"https://weatherapi-com.p.rapidapi.com/current.json?q={city1.GetCityName()}";
+            string url2 = $"https://weatherapi-com.p.rapidapi.com/current.json?q=Pretoria";
+            WeatherData weather = api.GetData(url);
+
+            return View(weather);
+        }
+    }
+}
---
+++ Data/API.cs
@@ -0,0 +1,20 @@
+﻿using WeatherSite.Models;
+
+namespace WeatherSite.Data
+{
+    public class API
+    {
+        public WeatherData GetData(string url)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-RapidAPI-Key", "c0e19bd40cmshd91a9f7f18f45eap101ac0jsneec7c637be67");
+            client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com");
+
+            WeatherData weatherData = client.GetFromJsonAsync<WeatherData>(url).Result;
+
+
+
+            return weatherData;
+        }
+    }
+}
